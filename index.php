ce1801b153f72654ded5b314672cdbc30c101bbc
Rafael <bl4anc@gmail.com>
2021-11-23 23:07:12 -0300

    .

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 set_time_limit(500);
 require_once "./vendor/autoload.php";
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
 date_default_timezone_set('America/Sao_Paulo');
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
